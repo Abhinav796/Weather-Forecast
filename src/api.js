cd375965d75bf41e26b7268aa0347f1b985c77d0
Abhinav796 <abhinavmishra01234@gmail.com>
2023-10-30 13:11:34 +0530

    added project files

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "b8869d97e8msh15dcefebe1d6950p131e19jsne6acdfa49bf9",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "91531406be621bf5a52c2b69f18a3c06"; // enter your key from openweather API
